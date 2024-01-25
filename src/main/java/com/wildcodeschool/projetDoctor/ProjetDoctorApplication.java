ae50f300fd0412c3b2a6bc8d3b12d5642d2016ec
AB <ab@AB.lan>
2024-01-25 14:46:16 +0100

    Hello, I'm the Doctor

---
+++ src/main/java/com/wildcodeschool/projetDoctor/ProjetDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.projetDoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/projetDoctor/ProjetDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.projetDoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
